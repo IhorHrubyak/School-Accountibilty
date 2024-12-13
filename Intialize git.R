a3bf1e67a9e8b46d511cf2153554119b829cc757
John Ihor Campagna <johncampagna2@gmail.com>
2024-12-13 12:50:46 -0500

    Initial commit

---
+++ Intialize git.R
@@ -0,0 +1,10 @@
+# Set project directory if needed
+# https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r
+setwd("C:/R Projects/Homebuyers and School Accountability")
+library(usethis)
+use_git()
+
+# In case need to get a new token
+# create_github_token()
+# library(gitcreds)
+# gitcreds_set() 
